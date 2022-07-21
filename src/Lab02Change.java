70ae1bf0e0be5fc105d50f90e1cd03319546e544
Doug Ryan <ryan2135@pacificu.edu>
2022-07-21 16:52:21 -0700

    Initial commit

---
+++ src/Lab02Change.java
@@ -0,0 +1,14 @@
+/*
+Filename:
+Author:
+Course:
+Assignment:
+Purpose:
+Hours:
+ */
+public class Lab02Change {
+  public static void main (String[] args) {
+    int pennies = 197; // Change this value to test your program
+
+  }
+}
